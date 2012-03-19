88891e4524fde21f851110c201dcda0a83338f79
Marco Lattarulo <marco.lattarulo@nexse.com>
2012-03-19 14:31:34 +0100

    aggiunto test-004

---
+++ kata-004/src/test/java/QueryStringComposerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class QueryStringComposerTest {
+
+/**
+ * TODO: creare una querystring a partire da coppie chiave valore:
+ * se non ce ne sono la query string deve essere ""
+ * se ce n'e' una deve essere "?k1=v1"
+ * se ce ne sono piu' di una "?k1=v1&k2=v2&...."
+ * se per qualche coppia la chiave k o il valore v e' nullo o vuoto, la coppia va omessa
+ *
+ * Per questo kata, essendo l'implementazione libera, va scritto anche il test
+ *
+ */
+
+}
