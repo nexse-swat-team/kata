9319e94c6c663a0c1f7cdf2e6a79bcd74219e68a
Mirko Caserta <mirko.caserta@gmail.com>
2012-03-30 12:42:32 +0200

    Added tests for kata-002 from Bruno's branch

--- kata-002/src/test/java/SoftwareVersionComparatorTest.java
+++ kata-002/src/test/java/SoftwareVersionComparatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 public class SoftwareVersionComparatorTest {
 
     private final SoftwareVersionComparator comparator = new SoftwareVersionComparator();
-    
+
     @Test
     public void testCompare() {
         assertTrue("1,1", comparator.compare("1", "1") == 0);
@@ -38,6 +38,13 @@ public class SoftwareVersionComparatorTest {
         assertTrue("3.1.678.34.9,3.0.677.33.8", comparator.compare("3.1.678.34.9", "3.0.677.33.8") > 0);
         assertTrue("45.1.678.34.9,3.0.677.33.8", comparator.compare("45.1.678.34.9", "3.0.677.33.8") > 0);
         assertTrue("5.1.678.34.9,6.0.677.33.8", comparator.compare("5.1.678.34.9", "6.0.677.33.8") < 0);
+
+        // bastard check
+        assertTrue("1,1.0", comparator.compare("1", "1.0") == 0);
+        // more bastard check
+        assertTrue("1.0.0,1", comparator.compare("1.0.0", "1") == 0);
+        // extreme bastard check
+        assertTrue("1.0.0.0.1.0,1", comparator.compare("1.0.0.0.1.0", "1") > 0);
     }
 
 }
