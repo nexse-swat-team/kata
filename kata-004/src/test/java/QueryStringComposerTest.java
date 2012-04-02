f3a453fbbea2a0fa0ce3891028adcab3dc794030
Marco Lattarulo <marco.lattarulo@nexse.com>
2012-04-02 14:34:55 +0200

    corretto test

--- kata-004/src/test/java/QueryStringComposerTest.java
+++ kata-004/src/test/java/QueryStringComposerTest.java
@@ -6,21 +6,20 @@ public class QueryStringComposerTest {
 
     @Test
     public void queryStringComposerTest(){
-        QueryStringComposer queryStringComposer = new QueryStringComposer();
-        assertEquals("?k1=v1", queryStringComposer.addPair("k1", "v1").toString());
-        assertEquals("", queryStringComposer.addPair(null, "v1").toString());
-        assertEquals("", queryStringComposer.addPair("k1", null).toString());
-        assertEquals("", queryStringComposer.addPair(null, null).toString());
-        assertEquals("", queryStringComposer.addPair("", "v1").toString());
-        assertEquals("", queryStringComposer.addPair("k1", "").toString());
-        assertEquals("", queryStringComposer.addPair("", "").toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").toString());
+        assertEquals("", new QueryStringComposer().addPair(null, "v1").toString());
+        assertEquals("", new QueryStringComposer().addPair("k1", null).toString());
+        assertEquals("", new QueryStringComposer().addPair(null, null).toString());
+        assertEquals("", new QueryStringComposer().addPair("", "v1").toString());
+        assertEquals("", new QueryStringComposer().addPair("k1", "").toString());
+        assertEquals("", new QueryStringComposer().addPair("", "").toString());
 
-        assertEquals("?k1=v1&k2=v2", queryStringComposer.addPair("k1", "v1").addPair("k2","v2").toString());
-        assertEquals("?k1=v1", queryStringComposer.addPair("k1", "v1").addPair(null, "v2").toString());
-        assertEquals("", queryStringComposer.addPair("k1", "v1").addPair("k2", null).toString());
-        assertEquals("", queryStringComposer.addPair("k1", "v1").addPair(null, null).toString());
-        assertEquals("", queryStringComposer.addPair("k1", "v1").addPair("", "v2").toString());
-        assertEquals("", queryStringComposer.addPair("k1", "v1").addPair("k2", "").toString());
-        assertEquals("", queryStringComposer.addPair("k1", "v1").addPair("", "").toString());
+        assertEquals("?k1=v1&k2=v2", new QueryStringComposer().addPair("k1", "v1").addPair("k2","v2").toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").addPair(null, "v2").toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").addPair("k2", null).toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").addPair(null, null).toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").addPair("", "v2").toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").addPair("k2", "").toString());
+        assertEquals("?k1=v1", new QueryStringComposer().addPair("k1", "v1").addPair("", "").toString());
     }
 }
